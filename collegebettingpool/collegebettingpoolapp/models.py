0938cb4f2b4ed0b71d2d8a5a8492f54ac17f7069
ehutch24 <ehutch24@gmail.com>
2018-10-25 13:15:17 -0500

    Update collegebettingpool/collegebettingpoolapp/models.py
    Added class to hold games - might rethink this...

--- collegebettingpool/collegebettingpoolapp/models.py
+++ collegebettingpool/collegebettingpoolapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -9,3 +10,14 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Game(models.Model):
+    favorite = models.CharField(max_length=200)
+    underdog = models.CharField(max_length=200)
+    line = models.DecimalField(null=true)
+    tv = models.CharField(max_length=20, null=true)
+    datetime = models.DateTimeField(null=true)
+    week = models.IntegerField(default=0)
+
+
