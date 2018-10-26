bcb462f302b0a0188403129ee8b944ac464917f5
wmoody1 <44455425+wmoody1@users.noreply.github.com>
2018-10-26 18:45:26 -0500

    Garrett-Login/Logout
    Added Login and Logout features. Edited the views to load a template html for logout. Text moved to html page 'home_page.html'

--- collegebettingpool/collegebettingpool/settings.py
+++ collegebettingpool/collegebettingpool/settings.py
@@ -121,3 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/collegebettingpoolapp/'
+LOGOUT_REDIRECT_URL = '/collegebettingpoolapp/'
--- collegebettingpool/collegebettingpoolapp/views.py
+++ collegebettingpool/collegebettingpoolapp/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-	return HttpResponse("The start of something great by the Super Software Bros.")
+	#return HttpResponse("The start of something great by the Super Software Bros.")
+	template = loader.get_template('collegebettingpool/home_page.html')
+	context = {
+
+	}
+	return HttpResponse(template.render(context, request))
