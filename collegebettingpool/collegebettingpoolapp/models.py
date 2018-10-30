a31dfe815c1a0eb92b635f7439bba06084bcb3b8
aaronralex <33357811+aaronralex@users.noreply.github.com>
2018-10-30 11:28:50 -0500

    Update

--- collegebettingpool/collegebettingpoolapp/models.py
+++ collegebettingpool/collegebettingpoolapp/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-
 class Game(models.Model):
     favorite = models.CharField(max_length=200)
     underdog = models.CharField(max_length=200)
@@ -45,3 +33,9 @@ class Participant(models.Model):
         return self.name
 
 
+class Bet(models.Model):
+    userID = models.IntegerField(default=0)
+    gameID = models.IntegerField(default=0)
+    week = models.IntegerField(default=0)
+    bet_selection = models.CharField(max_length=200)
+    winner = models.BooleanField()
--- collegebettingpool/collegebettingpoolapp/views.py
+++ collegebettingpool/collegebettingpoolapp/views.py
@@ -5,11 +5,19 @@ from django.views import generic
 from django.template import loader
 
 
-from .models import Game, BettingSheet, Participant
+from .models import Game, BettingSheet, Participant, Bet
 
 def index(request):
 	current_week_game_list = Game.objects.order_by('id')[:15]
 	context = {'current_week_game_list': current_week_game_list}
+
+	if requst.method == "POST":
+		userID = request.post['username']
+		userID.save()
+
+		for game in current_week_game_list
+			bet_selection = request.POST[game.id]
+			bet_selection.save()
 
 	return render(request, 'collegebettingpoolapp/home.html', context)
 
