79c715e63fa58364940fc2d4735da757b3447375
ehutch24 <ehutch24@gmail.com>
2018-10-30 14:44:01 -0500

    Update collegebettingpool/collegebettingpoolapp/admin.py

--- collegebettingpool/collegebettingpoolapp/admin.py
+++ collegebettingpool/collegebettingpoolapp/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Game
 admin.site.register(Game)
-from .models import BettingSheet
-admin.site.register(BettingSheet)
+from .models import Bet
+admin.site.register(Bet)
