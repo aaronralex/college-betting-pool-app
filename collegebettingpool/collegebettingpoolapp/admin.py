cbd6842dab160be92f5c4194a1730c9070ea0eba
ehutch24 <ehutch24@gmail.com>
2018-10-30 17:47:29 -0500

    Revert "Merge branch 'Garrett-Login/Logout' of https://github.com/aaronralex/college-betting-pool-app into Garrett-Login/Logout"
    This reverts commit 40c1f220103178144d9adaa324b092f39800957c, reversing
    changes made to a1be30520400b09068672f26a0b7394f40f09a72.

--- collegebettingpool/collegebettingpoolapp/admin.py
+++ collegebettingpool/collegebettingpoolapp/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Game, Bet
+from .models import Game
 admin.site.register(Game)
-<<<<<<< HEAD
 from .models import BettingSheet
 admin.site.register(BettingSheet)
 from .models import Bet
 admin.site.register(Bet)
-=======
-admin.site.register(Bet)
->>>>>>> 736fe240627c2539a63759ecc8e57d082bba2f2c
--- collegebettingpool/collegebettingpoolapp/views.py
+++ collegebettingpool/collegebettingpoolapp/views.py
@@ -4,14 +4,13 @@ from django.urls import reverse
 from django.views import generic
 from django.template import loader
 
-from .models import Game, Bet
 
+from .models import Game, BettingSheet, Participant, Bet
 
 def index(request):
-    current_week_game_list = Game.objects.order_by('id')[:15]
-    context = {'current_week_game_list': current_week_game_list}
+	current_week_game_list = Game.objects.order_by('id')[:15]
+	context = {'current_week_game_list': current_week_game_list}
 
-<<<<<<< HEAD
 	if request.method == "POST":
 		userID = request.POST["username"]
 
@@ -19,17 +18,8 @@ def index(request):
 			bet_selection = request.POST["favorite"]
 			bet = Bet(1, 2, 1, 0, bet_selection)
 			bet.save()
-=======
-    if request.method == "POST":
-        
 
-        for game in current_week_game_list:
-            bet_selection = request.POST[game.id]
-            bet_selection.save()
-
-    return render(request, 'collegebettingpoolapp/home.html', context)
->>>>>>> 736fe240627c2539a63759ecc8e57d082bba2f2c
-
+	return render(request, 'collegebettingpoolapp/home.html', context)
 
 def about(request):
-    return render(request, 'collegebettingpoolapp/about.html')
+	return render(request, 'collegebettingpoolapp/about.html')
