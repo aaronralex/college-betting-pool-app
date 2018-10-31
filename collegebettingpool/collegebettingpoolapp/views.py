c30c48d35c07b56406e6add92333e3c231442e37
ehutch24 <ehutch24@gmail.com>
2018-10-31 18:48:43 -0500

    update

--- collegebettingpool/collegebettingpoolapp/views.py
+++ collegebettingpool/collegebettingpoolapp/views.py
@@ -4,11 +4,12 @@ from django.urls import reverse
 from django.views import generic
 from django.template import loader
 
-from .models import Game, Bet
+from .models import Game, Bet, Setting
 
 
 def index(request):
-    current_week_game_list = Game.objects.filter(week=1).order_by('id')[:15]
+    current_week = Setting.objects.get(setting="CurrentWeek")
+    current_week_game_list = Game.objects.filter(week=current_week.value).order_by('id')[:15]
     context = {'current_week_game_list': current_week_game_list}
 
     if request.method == "POST":
@@ -22,11 +23,11 @@ def index(request):
                 winner = False
             try:
                 select_object = Bet.objects.get(userID=user_id, gameID=game.id)
-                b = Bet(id=select_object.id, userID=user_id, gameID=game.id, week=1,
+                b = Bet(id=select_object.id, userID=user_id, gameID=game.id, week=current_week.value,
                         winner=winner, bet_selection=selection)
                 b.save()
             except Bet.DoesNotExist:
-                b = Bet(userID=user_id, gameID=game.id, week=1, winner=winner,
+                b = Bet(userID=user_id, gameID=game.id, week=current_week.value, winner=winner,
                         bet_selection=selection)
                 b.save()
 
