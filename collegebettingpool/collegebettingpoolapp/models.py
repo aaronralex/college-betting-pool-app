06c8920e36a714952ffbf4fe4dfd19e6525245ae
ehutch24 <ehutch24@gmail.com>
2018-11-08 16:47:45 -0600

    Score Model

--- collegebettingpool/collegebettingpoolapp/models.py
+++ collegebettingpool/collegebettingpoolapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Game(models.Model):
     favorite = models.CharField(max_length=200)
     underdog = models.CharField(max_length=200)
@@ -11,6 +12,7 @@ class Game(models.Model):
     datetime = models.DateTimeField(default=0)
     week = models.IntegerField(default=0)
     game_of_the_week = models.BooleanField(default=False)
+    game_winner = models.NullBooleanField()
 
     def __str__(self):
         return self.favorite + ' vs ' + self.underdog
@@ -29,7 +31,7 @@ class Bet(models.Model):
     def __str__(self):
         u = User.objects.get(id=self.userID)
         name = u.first_name
-        if(name == ""):
+        if name == "":
             name = u.username
         return "Bet for User: " + name + "; Game: " + str(self.gameID)
 
@@ -48,12 +50,17 @@ class Participant(models.Model):
     total_points = models.IntegerField(default=0)
     has_paid = models.BooleanField(default=False)
 
-
     @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
+    def create_user_profile(self, instance, created, **kwargs):
         if created:
             Participant.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
+    def save_user_profile(self, instance, **kwargs):
         instance.participant.save()
+
+
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    week = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
