1211015799a6d1058dc7bc1315bd4959c95fe7ee
wmoody1 <44455425+wmoody1@users.noreply.github.com>
2018-11-10 14:03:17 -0600

    First push

--- collegebettingpool/collegebettingpoolapp/views.py
+++ collegebettingpool/collegebettingpoolapp/views.py
@@ -10,11 +10,21 @@ from .models import Game, Bet, Setting, Participant
 def index(request):
     current_week = Setting.objects.get(setting="CurrentWeek")
     current_week_game_list = Game.objects.filter(week=current_week.value).order_by('id')[:15]
-    context = {'current_week_game_list': current_week_game_list}
+    user_id = request.user.id
+    try:
+        current_week_game_choices = Bet.objects.filter(userID=user_id, week=current_week.value)
+
+    except Bet.DoesNotExist:
+        current_week_game_choices = ['null msg', 'null msg']
+
+
+
 
     if request.method == "POST":
         user_id = request.POST["userID"]
         game_of_the_week_points = request.POST["game_of_the_week_points"]
+
+
 
         for game in current_week_game_list:
             selection = request.POST["g" + str(game.id)]
@@ -30,6 +40,15 @@ def index(request):
             except Bet.DoesNotExist:
                 b = Bet(userID=user_id, gameID=game.id, week=current_week.value, winner=winner)
                 b.save()
+
+    # else:
+    #     #user_id = request.POST["userID"]
+    #     current_week_game_choices = Bet.objects.filter(userID=9)
+
+    context = {
+    	'current_week_game_list': current_week_game_list,
+    	'current_week_game_choices': current_week_game_choices
+    }
 
     return render(request, 'collegebettingpoolapp/home.html', context)
 
